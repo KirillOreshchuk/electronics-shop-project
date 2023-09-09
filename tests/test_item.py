ab519c1a520a9f922dc237c1e59dd7dd25652873
KirillOreshchuk <oreshchuk.kirill@gmail.com>
2023-09-09 23:59:49 +1000

    fix test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,14 +2,19 @@
 import pytest
 from src.item import Item
 
+
 item3 = Item("Холодильник", 30000, 10)
+
+
 def test_item_init():
 
     assert item3.name == "Холодильник"
     assert item3.price == 30000
 
+
 def test_calculate_total_price():
     assert item3.calculate_total_price() == 300000
+
 
 def test_apply_discount():
     Item.pay_rate = 0.5
