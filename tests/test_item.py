2a3f0739f35fdf485773d3f717b8e0ef420ba885
KirillOreshchuk <oreshchuk.kirill@gmail.com>
2023-09-10 21:30:45 +1000

    fix test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,19 +3,21 @@ import pytest
 from src.item import Item
 
 
-item3 = Item("Холодильник", 30000, 10)
+@pytest.fixture()
+def item3():
+    return Item("Холодильник", 30000, 10)
 
 
-def test_item_init():
+def test__init__(item3):
 
     assert item3.name == "Холодильник"
     assert item3.price == 30000
 
 
-def test_calculate_total_price():
-    assert item3.calculate_total_price() == 300000
+def test_calculate_total_price(item3):
+    assert Item.calculate_total_price(item3) == 300000
 
 
-def test_apply_discount():
+def test_apply_discount(item3):
     Item.pay_rate = 0.5
-    assert item3.apply_discount() == 15000
+    assert Item.apply_discount(item3) == 15000
