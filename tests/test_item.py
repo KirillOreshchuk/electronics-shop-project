4ed69063fee8abf10c1b9708899f1c81e29db5a6
KirillOreshchuk <oreshchuk.kirill@gmail.com>
2023-09-12 21:31:36 +1000

    homework2: modified item.py, test_item.py, ; fix main.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,6 +3,7 @@ import pytest
 from src.item import Item
 
 
+
 @pytest.fixture()
 def item3():
     return Item("Холодильник", 30000, 10)
@@ -21,3 +22,9 @@ def test_calculate_total_price(item3):
 def test_apply_discount(item3):
     Item.pay_rate = 0.5
     assert Item.apply_discount(item3) == 15000
+
+
+def test_name(item3):
+    assert item3.name == "Холодильник"
+    item3.name = "Будильник"
+    assert item3.name == "Будильник"
